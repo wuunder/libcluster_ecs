7eac33c4b6162f8822b129791b2e938b205b1b96
Derek Kraan <derek@kraan.dev>
2022-09-15 13:28:48 +0200

    Get ready to release 0.1

--- mix.exs
+++ mix.exs
@@ -7,20 +7,32 @@ defmodule ClusterEcs.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      name: "Libcluster ECS",
+      package: package(),
+      source_url: "https://github.com/wuunder/libcluster_ecs",
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp package do
+    [
+      description: "libcluster + AWS Elastic Container Service (ECS)",
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/wuunder/libcluster_ecs"
+      }
+    ]
+  end
+
   defp deps do
     [
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:ex_aws, "~> 2.1"},
       {:ex_aws_ec2, "~> 2.0"},
       {:sweet_xml, "~> 0.7.0"}
