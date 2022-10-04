3af02230913d28012cea5b24604eeec7a86b932f
Timmo Verlaan <timmo@timmo.immo>
2022-10-04 14:16:00 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClusterEcs.MixProject do
   def project do
     [
       app: :libcluster_ecs,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       name: "Libcluster ECS",
